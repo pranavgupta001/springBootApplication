69ee2ad695c90bfa008dd109e080b2ca67036d1e
Akash Suryavanshi <96387070+asv02@users.noreply.github.com>
2023-07-31 22:47:05 +0530

    Add files via upload

---
+++ TruckBooking/src/main/java/com/TruckBooking/ContractRateUpload/Entity/Rates.java
@@ -0,0 +1,49 @@
+package com.TruckBooking.ContractRateUpload.Entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="Contract_Rates")
+public class Rates {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NotBlank(message = "Loading Point Cannot Be Empty")
+    @Column(name = "loadingPoint")
+    private String loadingPoint;
+
+    @NotBlank(message = "UnLoading Point Cannot Be Empty")
+    @Column(name = "unLoadingPoint")
+    private String unLoadingPoint;
+
+    @NotNull(message = "Weight Cannot Be Empty")
+    @Column(name = "weight")
+    private Integer weight;
+
+    @NotNull(message = "Rates Cannot Be Empty")
+    @Column(name = "rate")
+    private Integer rate;
+
+    @Column(name = "TransporterId")
+    private Integer TransporterId;
+
+    @NotBlank(message = "Transporter Name Cannot Be Empty")
+    @Column(name = "TransporterName")
+    private String TransporterName;
+
+    @NotBlank(message = "Transporter Email Cannot Be Empty")
+    @Column(name="TransporterEmail")
+    private String TransporterEmail;
+}
