f2a44a1d74fbdbd329e3300d65852cf378d9bc84
jayshree2419 <78597325+jayshree2419@users.noreply.github.com>
2023-10-04 22:19:36 +0530

    Update ULIPController.java

--- TruckBooking/src/main/java/com/TruckBooking/ULIP/Controller/ULIPController.java
+++ TruckBooking/src/main/java/com/TruckBooking/ULIP/Controller/ULIPController.java
@@ -13,9 +13,10 @@ import com.TruckBooking.ULIP.Entity.FastagEntity;
 import com.TruckBooking.ULIP.Entity.SarathiEntity;
 import com.TruckBooking.ULIP.Entity.VahanEntity;
 import com.TruckBooking.ULIP.Service.ULIPService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
-
+@CrossOrigin
 @RestController
 public class ULIPController {
 	
