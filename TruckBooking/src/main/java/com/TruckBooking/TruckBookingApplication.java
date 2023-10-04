5d4f58ec9e3712d454c0127f093f2663cc633539
jayshree2419 <78597325+jayshree2419@users.noreply.github.com>
2023-10-04 22:19:10 +0530

    Update TruckBookingApplication.java

--- TruckBooking/src/main/java/com/TruckBooking/TruckBookingApplication.java
+++ TruckBooking/src/main/java/com/TruckBooking/TruckBookingApplication.java
@@ -3,9 +3,8 @@ package com.TruckBooking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin
+
 @SpringBootApplication
 @EnableScheduling
 public class TruckBookingApplication {
