786af7497b9238db4945c47f7f51a206bf153287
rupalisingh12 <rupalisinghkumr12@gmail.com>
2023-11-09 00:26:49 +0530

    just removed cros origin from main class

--- TruckBooking/src/main/java/com/TruckBooking/TruckBookingApplication.java
+++ TruckBooking/src/main/java/com/TruckBooking/TruckBookingApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin
+
 @SpringBootApplication
 @EnableScheduling
 public class TruckBookingApplication {
