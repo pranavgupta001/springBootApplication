50892d47d71392380d0bbb3ea03449e3a0380685
anush.hash <saxenaanush88@gmail.com>
2024-01-15 18:44:32 +0530

    Placement Efficiency Graph feature Added/Completed

--- TruckBooking/src/main/java/com/TruckBooking/Analytics/placementEfficiencyGraph/Controller/PlacementController.java
+++ TruckBooking/src/main/java/com/TruckBooking/Analytics/placementEfficiencyGraph/Controller/PlacementController.java
@@ -1,13 +1,11 @@
 package com.TruckBooking.Analytics.placementEfficiencyGraph.Controller;
 
-import com.TruckBooking.Analytics.placementEfficiencyGraph.Entities.Placements;
 import com.TruckBooking.Analytics.placementEfficiencyGraph.Service.PlacementService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -16,13 +14,12 @@ public class PlacementController {
     @Autowired
     private PlacementService placementService;
 
-    @GetMapping("/placementGraph/{id}") // the id will either be ShipperId or TransporterId
-    public ResponseEntity<Object> graphData(@PathVariable String id){
+    @GetMapping("/placementEfficiencyGraph")
+    public ResponseEntity<?> placementG(@RequestParam String id){   // id - common for both transporterId and shipperId
         Object response = placementService.getPlacementData(id);
-        if (response.getClass() == "".getClass()){  // looking if object returned is string or entity
+        if (response.getClass().equals("".getClass())){    // checking if the response returned is String or Not.
             return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
-
 }
--- TruckBooking/src/main/java/com/TruckBooking/Analytics/placementEfficiencyGraph/Service/PlacementServiceImpl.java
+++ TruckBooking/src/main/java/com/TruckBooking/Analytics/placementEfficiencyGraph/Service/PlacementServiceImpl.java
@@ -2,12 +2,13 @@ package com.TruckBooking.Analytics.placementEfficiencyGraph.Service;
 
 import com.TruckBooking.Analytics.placementEfficiencyGraph.Dao.PlacementDao;
 import com.TruckBooking.Analytics.placementEfficiencyGraph.Entities.PlacementInfo;
-import com.TruckBooking.Analytics.placementEfficiencyGraph.Entities.Placements;
+import com.TruckBooking.Analytics.placementEfficiencyGraph.Model.PlacementEfficiencyResponse;
 import com.TruckBooking.Booking.Dao.BookingDao;
 import com.TruckBooking.Booking.Entities.BookingData;
 import com.TruckBooking.ContractRateUpload.Dao.ContractRateRepo;
 import com.TruckBooking.ContractRateUpload.Dao.IndentDao;
 import com.TruckBooking.ContractRateUpload.Entity.Indent;
+import com.TruckBooking.ContractRateUpload.Entity.Rates;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -19,164 +20,172 @@ import java.util.*;
 @Service
 public class PlacementServiceImpl implements PlacementService{
 
-    @Autowired
-    private BookingDao bookingDao;
+    private static Timestamp lastSchedulerTimestamp = Timestamp.from(Instant.now());
 
     @Autowired
-    private PlacementDao placementDao;
+    private BookingDao bookingDao;
 
     @Autowired
     private IndentDao indentDao;
 
     @Autowired
-    private ContractRateRepo contractRateRepo;
+    private PlacementDao placementDao;
 
-    private static Timestamp lastSchedulerTimestamp = Timestamp.from(Instant.now());
-    //storing the last timestamp at which scheduler ran
+    @Autowired
+    private ContractRateRepo contractRateRepo;
 
     @Override
     public Object getPlacementData(String id){
         if (id.charAt(0) == 't'){ //checking if id given is transporterId or shipperId  since ids begin with the respective id name
-            // transporterId = "transporter: heX codE"
-            Optional<Placements> optional = placementDao.findById(id);
+            // transporterId = "transporter:heX codE"
+            Optional<PlacementInfo> optional = placementDao.findById(id);
             if (optional.isPresent()){
-                Placements responsePlacement = optional.get();
-                responsePlacement.setPlacementInfoList(List.of(responsePlacement.getPlacementInfoList().get(0)));
-                return responsePlacement;
+
+                return setResponse(optional.get());  // returning the calculated efficiency response.
             }
-            else return "TransporterId does not exists.";
+            else return "TransporterId does not exists.";    // If transporterId is wrong or not found
         }
         else{
-            Optional<List<Placements>> optional = placementDao.findByShipperId(id);
+            // shipperId = "shipperId:heX codE"
+            Optional<List<PlacementInfo>> optional = placementDao.findByShipperId(id);
             if (optional.isPresent()){
-                List<Placements> list = optional.get();
-                for (Placements x: list){
-                    // since there are duplicate entries therefore to get any entry we get the corresponding one
-                    x.setPlacementInfoList(List.of(x.getPlacementInfoList().get(0)));
+
+                List<PlacementEfficiencyResponse> responseList = new ArrayList<>();
+                for (PlacementInfo info: optional.get()){
+                    responseList.add(setResponse(info));      // adding in a list since all the transporter belong to this particular shipper.
                 }
-                return list;
+                return responseList;
             }
-            else return "ShipperId does not exists";
+            else return "ShipperId does not exists";          // If shipperId is wrong or not found
         }
     }
 
-    public void createNewPlacement(Placements placements, List<PlacementInfo> placementInfoList, int bookingYear, int bookingMonth, int bookingWeek, int index){
-        // Method for creating a new Placement since all the data types are primitive therefore this method is very helpful
-        //placementList for adding in PlacementInfo
-        List<Integer> placementList = new ArrayList<>();
-        placementList.add(0);
-        placementList.add(0);
-        placementList.add(0);
-        placementList.add(0);
-        placementList.add(0);
-        placementList.set(index,1);
+    public PlacementEfficiencyResponse setResponse(PlacementInfo info){
 
-        PlacementInfo newPlacementInfo = new PlacementInfo(bookingYear,bookingMonth,bookingWeek,placementList);
-        placementInfoList.add(newPlacementInfo);
-        placements.setPlacementInfoList(placementInfoList);
+        PlacementEfficiencyResponse response = new PlacementEfficiencyResponse();
+        HashMap<String, float[]> responseMap = new HashMap<>();                            // creating entities for response.
 
+        Map<String, String> map= info.getPlacementMap();                           // actual map containing data.
+
+        for (String str: map.keySet()){
+
+            String[] arr = map.get(str).substring(1,map.get(str).length()-1).split(", ");
+            // Above line will convert from  "[12, 43, 75, 87, 9]"   to ["12", "43", "75", "87", "9"]
+            // Basically converting Stringified array to an array of String  to get these values as Numbers.
+
+            float[] responseArr = new float[arr.length];           // initializing a float array to store placement efficiency percentage.
+
+            float total=0;
+            for (int i=0; i<arr.length; i++){
+                responseArr[i]=Float.parseFloat(arr[i]);           // converting each element of String array "arr" to float and calculating sum/total deliveries.
+                total+=responseArr[i];
+            }
+            for (int i=0; i<arr.length; i++){
+                responseArr[i]= (responseArr[i]/total)*100;        // Taking out and storing percentage for each time duration.
+            }
+
+            responseMap.put(str, responseArr);
+        }
+
+        response.setTransporterId(info.getTransporterId());
+        response.setTransporterName(info.getTransporterName());
+        response.setShipperId(info.getShipperId());
+        response.setPlacementEfficiencyMap(responseMap);           // setting up all the fields of response
+        return response;
     }
 
     @Scheduled(fixedDelay = 60000)
-    public void FindPlacement(){
-        //Scheduler for calculating the placement info
-//        calculate the bookings remaining after the previous timeStamp
+    public void FindPlacement() {
         List<BookingData> bookingDataList = bookingDao.findByTimestampIsBefore(lastSchedulerTimestamp);
         lastSchedulerTimestamp = Timestamp.from(Instant.now());
 
-        if (!bookingDataList.isEmpty()){
+        if (!bookingDataList.isEmpty()) {
             System.out.println("Bookings are Found");
 
-            List<PlacementInfo> placementInfoList;
-            List<Integer> placementList;
-            PlacementInfo placementInfo;
-
-            for (BookingData bookingData: bookingDataList){
+            for (BookingData bookingData : bookingDataList) {
                 // getting transporterId from booking Data
                 // shipperId from contractService through transporterId
                 // these ids will be stored.
                 String transporterId = bookingData.getTransporterId();
-                String shipperId = contractRateRepo.findByTransporterId(transporterId).getShipperId();
+                Rates rates = contractRateRepo.findByTransporterId(transporterId);
+                String shipperId = "Shipper Name";//rates.getShipperId();
+                String transporterName = "Transporter Name";//rates.getTransporterName();
 
                 String loadId = bookingData.getLoadId(); // to get the data from indent table load Id is reqd.
                 Timestamp bookingTimestamp = bookingData.getTimestamp();
                 long bookingMillis = bookingTimestamp.getTime(); // getting bookingMillis to make booking calendar and find the year, month, week of booking
                 Calendar bookingCalendar = Calendar.getInstance();
                 bookingCalendar.setTimeInMillis(bookingMillis);
-                int bookingYear = bookingCalendar.get(Calendar.YEAR);
-                int bookingMonth = bookingCalendar.get(Calendar.MONTH);
-                int bookingWeek = bookingCalendar.get(Calendar.WEEK_OF_MONTH);
+                String bookingYear = "year: " + bookingCalendar.get(Calendar.YEAR);
+                String bookingMonth = "month: " +( 1+ bookingCalendar.get(Calendar.MONTH)); // adding 1 since monthNumber begins with 0 i.e. 0 -> Jan
+                String bookingWeek = "week: " + bookingCalendar.get(Calendar.WEEK_OF_YEAR);
 
                 Indent indent = indentDao.findByLoadId(loadId);
 
-                Timestamp indentTimestamp;
-                indentTimestamp = indent.getAssignedTime();
-                long  indentMillis = indentTimestamp.getTime(); // getting indentMillis to calculate the time difference between
-                                                            // load assigning to the transporter and transporter assigning the vehicle for load
-                int millisInAnHour = 3600000;  // millis in 1 hour
+                int index = getIndex(indent.getAssignedTime(), bookingMillis);
 
-                int index ; // index for the list at which we will add the booking
-                long difference = bookingMillis-indentMillis;
-                if (difference <= 5*millisInAnHour){  // 0-5 hours
-                    index=0;
-                }
-                else if (difference <= 10*millisInAnHour){// 5-10 hours
-                    index=1;
-                }
-                else if (difference <= 15*millisInAnHour){  // 10-15 hours
-                    index=2;
-                }
-                else if (difference <= 20*millisInAnHour){  // 15-20 hrs
-                    index=3;
-                }
-                else{
-                    index=4;    // >20 hours
+                Optional<com.TruckBooking.Analytics.placementEfficiencyGraph.Entities.PlacementInfo> optional = placementDao.findById(transporterId); // finding if the placement for this transporter existed before or not
+                PlacementInfo placementInfo;
+                if (optional.isPresent()) {
+                    placementInfo = optional.get();
+                    Map<String, String> placementMap = placementInfo.getPlacementMap();
+
+                    setArray(bookingYear, index, placementMap);
+                    setArray(bookingMonth, index, placementMap);
+                    setArray(bookingWeek, index, placementMap);
+
+                } else {
+                    placementInfo = new PlacementInfo();
+                    HashMap<String, String> placementMap = new HashMap<>();
+                    int[] placementArray = new int[5];
+                    placementArray[index] = 1;
+                    placementMap.put(bookingYear, Arrays.toString(placementArray));
+                    placementMap.put(bookingMonth, Arrays.toString(placementArray));
+                    placementMap.put(bookingWeek, Arrays.toString(placementArray));
+                    placementInfo.setPlacementMap(placementMap);
+                    placementInfo.setShipperId(shipperId);
+                    placementInfo.setTransporterId(transporterId);
+                    placementInfo.setTransporterName(transporterName);
                 }
 
-                Optional<Placements> optional= placementDao.findById(transporterId); // finding if the placement for this transporter existed before or not
-                Placements placements;
-                if (optional.isPresent()){
-
-                    placements = optional.get();
-                    int at = placements.getLastPosition();
-                    placementInfoList = placements.getPlacementInfoList();
-                    placementInfo = placementInfoList.get(at);            // Info for the last placement
-                    int lastPlacementYear = placementInfo.getYear();
-                    int lastPlacementMonth = placementInfo.getMonth();
-                    int lastPlacementWeek = placementInfo.getWeek();
-                    // getting time of last accepted booking week to compare for the current booing week
-
-                    if (lastPlacementYear == bookingYear){
-                        if (lastPlacementMonth == bookingMonth){
-                            if (lastPlacementWeek == bookingWeek){
-                                placementList = placementInfo.getPlacementList();
-                                placementList.set(index,placementList.get(index)+1);
-                                placementInfo.setPlacementList(placementList);
-                            }
-                            else{
-                                createNewPlacement(placements,placementInfoList,bookingYear,bookingMonth,bookingWeek,index);
-                            }
-                        }
-                        else{
-                            createNewPlacement(placements,placementInfoList,bookingYear,bookingMonth,bookingWeek,index);
-                        }
-                    }
-                    else{
-                        createNewPlacement(placements,placementInfoList,bookingYear,bookingMonth,bookingWeek,index);
-                    }
-                }
-                else{
-                    placements = new Placements();
-                    placementInfoList = new ArrayList<>();
-
-                    createNewPlacement(placements, placementInfoList, bookingYear, bookingMonth, bookingWeek, index);
-
-                    placements.setTransporterId(transporterId);
-                    placements.setShipperId(shipperId);
-                    placements.setLastPosition(0);
-                }
-                placementDao.save(placements);
+                placementDao.save(placementInfo);
             }
+        }
+    }
+
+    private int getIndex(Timestamp indentTimestamp, long bookingMillis) {
+        long indentMillis = indentTimestamp.getTime(); // getting indentMillis to calculate the time difference between
+        // load assigning to the transporter and transporter assigning the vehicle for load\
+
+        int millisInAnHour = 3600000;  // millis in 1 hour
+        int index; // index for the list at which we will add the booking
+        long difference = bookingMillis - indentMillis;
+
+        if (difference <= 5 * millisInAnHour) {  // 0-5 hours
+            index = 0;
+        } else if (difference <= 10 * millisInAnHour) {// 5-10 hours
+            index = 1;
+        } else if (difference <= 15 * millisInAnHour) {  // 10-15 hours
+            index = 2;
+        } else if (difference <= 20 * millisInAnHour) {  // 15-20 hrs
+            index = 3;
+        } else {
+            index = 4;    // >20 hours
+        }
+
+        return index;
+    }
+
+    private void setArray(String bookingTime, int index, Map<String, String> placementMap) {
+        int[] placementArray;
+        if (placementMap.containsKey(bookingTime)) {
+            String[] arr = placementMap.get(bookingTime).substring(1,placementMap.get(bookingTime).length()-1).split(", ");
+            arr[index] = Integer.parseInt(arr[index])+1+"";
+            placementMap.put(bookingTime, Arrays.toString(arr));
+        } else {
+            placementArray = new int[5];
+            placementArray[index] = 1;
+            placementMap.put(bookingTime, Arrays.toString(placementArray));
         }
     }
 }
