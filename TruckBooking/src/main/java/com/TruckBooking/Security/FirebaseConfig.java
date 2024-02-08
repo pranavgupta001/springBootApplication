f65cb8cdcdd79c57c858a6ad39d3fc980ad217db
Shravan-Parikh <102890682+Shravan-Parikh@users.noreply.github.com>
2024-02-08 15:12:31 +0530

    changed annotation of the class to solve AWS error

--- TruckBooking/src/main/java/com/TruckBooking/Security/FirebaseConfig.java
+++ TruckBooking/src/main/java/com/TruckBooking/Security/FirebaseConfig.java
@@ -6,12 +6,13 @@ import com.google.firebase.FirebaseOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.io.InputStream;
 
-@SpringBootApplication
+@Configuration
 public class FirebaseConfig {
 
 	@Value("${firebase.credentials.file}")
