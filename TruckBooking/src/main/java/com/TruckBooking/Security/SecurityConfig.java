5af2214672e66d88960bad6361a50c7138484acb
jayshree2419 <78597325+jayshree2419@users.noreply.github.com>
2024-02-13 20:29:04 +0530

    Update SecurityConfig.java

--- TruckBooking/src/main/java/com/TruckBooking/Security/SecurityConfig.java
+++ TruckBooking/src/main/java/com/TruckBooking/Security/SecurityConfig.java
@@ -7,9 +7,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebSecurity
+@EnableWebMvc
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	public static final String[] PUBLIC_URLS = {
